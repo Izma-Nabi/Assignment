f442af793fb19e41dac2b469222c2882b82f85dd
Izma <l232565@lhr.nu.edu.pk>
2026-09-05 20:03:25 +0500

    Question 1 Completed

--- src/LogForge.java
+++ src/LogForge.java
@@ -1,13 +1,75 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class LogForge {
+
+    public static void main(String[] args) {
+//        if (args.length < 1) {
+//            System.out.println("Usage: java LogForge system.log");
+//            return;
+//        }
+        FileReader fr = null;
+        BufferedReader br = null;
+        int total = 0;
+        int info = 0;
+        int warn = 0;
+        int error = 0;
+
+//        File filePath = new File(args[0]);
+        File filePath = new File("system.log");
+        try {
+            fr = new FileReader(filePath);
+            br = new BufferedReader(fr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                total++;
+                String level = getLevel(line);
+                if (level.equals("INFO")) {
+                    info++;
+                }
+                if (level.equals("ERROR")) {
+                    error++;
+                }
+                if (level.equals("WARN")) {
+                    warn++;
+                }
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+            return;
+        }
+
+        System.out.println("Total records: " + total);
+        System.out.println("INFO: " + info);
+        System.out.println("WARN: " + warn);
+        System.out.println("ERROR: " + error);
+    }
+
+    public static String getLevel(String line) {
+        int pipeCount = 0;
+        int start = -1;
+        int end = -1;
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '|') {
+                pipeCount++;
+                if (pipeCount == 2) {
+                    start = i + 1;
+                }
+                if (pipeCount == 3) {
+                    end = i;
+                }
+            }
+        }
+        String result = "";
+        if (start != -1 && end != -1) {
+            for (int i = start; i < end; i++) {
+                result += line.charAt(i);
+            }
+        }
+        return result;
     }
 }
+
